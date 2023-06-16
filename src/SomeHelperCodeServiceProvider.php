4b38aabe549fcedec3991252fa481e03385ef911
Mr.VACO <brute1904@gmail.com>
2023-06-16 12:36:01 +0500

    Added RU localization for statuses

---
+++ src/SomeHelperCodeServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MrVaco\SomeHelperCode;
+
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\ServiceProvider;
+
+class SomeHelperCodeServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Lang::addJsonPath(__DIR__ . '/Lang');
+    }
+}
